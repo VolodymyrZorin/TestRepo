95f7af558914c957a78018d4a67e30c0a0570cbf
Volodymyr Zorin <Volodymyr_Zorin@epam.com>
2014-06-05 15:16:00 +0300

    Fixed project

---
+++ src/stack/MyStack.java
@@ -0,0 +1,60 @@
+package stack;
+
+import java.util.EmptyStackException;
+
+/**
+ *
+ * @author Andrii_Rodionov
+ */
+
+public class MyStack {
+	
+    private Integer[] arr;    
+    private int size;
+    private final int MAX_STACK_SIZE;
+    
+    public MyStack(){
+       this(10);
+    }
+    
+    public MyStack(int stack_size){
+        MAX_STACK_SIZE = stack_size;
+        arr = new Integer[MAX_STACK_SIZE];
+        size = 0;//edit
+    }
+    
+    public Integer push(Integer item){
+        if(size == MAX_STACK_SIZE){
+            throw new IndexOutOfBoundsException();
+        }
+        arr[size] = item;//edit
+        size++;//edit
+        return item;
+    }  
+    
+    public Integer peek(){
+        if(empty()){
+            throw new EmptyStackException();
+        }
+        else{
+        return arr[size-1];//edit
+        }
+    }
+    
+    public Integer pop(){
+    	int buffer;//edit
+        if(empty()){
+            throw new EmptyStackException();
+        }
+        else{
+        	buffer = arr[size-1];//edit 
+        	arr[size-1] = 0;//edit
+            size--;//edit
+        return buffer;//edit
+        }
+    }
+    
+    public boolean empty(){
+        return (size == 0);
+    }
+}
---
+++ test/stack/TestMyStack.java
@@ -0,0 +1,75 @@
+package stack;
+
+import static org.junit.Assert.*;
+import java.util.EmptyStackException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMyStack {
+	
+	static void pln(String s) {
+		System.out.println(s);
+		}
+	
+	static MyStack TestArray;
+		
+	@Before
+	public void setUp() throws Exception {
+		TestArray = new MyStack();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		TestArray = null;
+	}
+
+	@Test
+	public void testStackEmpty() {
+		TestArray = new MyStack(0);
+		assertTrue(TestArray.empty());
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testPushExeption() {
+		int index = 20;
+		TestArray = new MyStack(index);
+		for (int i = 0;; i++){
+			TestArray.push(i);
+		}
+	}
+	@Test
+	public void testPush() {
+		int arrElement = TestArray.push(10);
+		assertEquals(10, arrElement);
+	}
+	
+	@Test
+	public void testPeek() {
+		for (int i = 0; i<=5; i++){
+			TestArray.push(i);
+		}
+		int arrEnd = TestArray.peek();
+		assertEquals(5,arrEnd);
+	}
+	
+	@Test
+	public void testPop() {
+		for (int i = 0; i <= 5; i++){
+			TestArray.push(i);
+		}
+		
+		int lastElementPeek = TestArray.peek();
+		int LastelementPop = TestArray.pop();
+		assertEquals(lastElementPeek, LastelementPop);
+		assertFalse(lastElementPeek==TestArray.peek());
+	}
+	
+	@Test (expected=EmptyStackException.class)
+	public void testPopExeption() {
+		for (int i = 0;; i++){
+			TestArray.pop();
+		}
+	}
+	
+}
